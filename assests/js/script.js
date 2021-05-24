7c1965b88c311197bbe3d2d4db35a6dbf9faceba
Andrew Bianco <andrewbianco98@gmail.com>
2021-05-23 21:00:24 -0400

    Finished the local storage and fixed current time issue, rework HTML to be easier to access

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,60 +1,56 @@
-// GIVEN I am using a daily planner to create a schedule DONE
-// WHEN I open the planner DONE
-// THEN the current day is displayed at the top of the calendar DONE
-// WHEN I scroll down DONE
-// THEN I am presented with timeblocks for standard business hours DONE
-// WHEN I view the timeblocks for that day DONE
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future DONE
-// WHEN I click into a timeblock DONE
-// THEN I can enter an event DONE
-// WHEN I click the save button for that timeblock DONE
-// THEN the text for that event is saved in local storage DONE
-// WHEN I refresh the page
-// THEN the saved events persist
+var buttons = document.querySelector(".container");
 var currentHour = moment().format("H");
 var currentDay = moment();
-$("#currentDay").text(currentDay.format("dddd MMMM do"));
+$("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
 
-var container = document.querySelector(".container");
-var plannerText = document.querySelector("plannerText");
-// var studentGrades = JSON.parse(localStorage.getItem("studentGrades")) || [];
-var todos = JSON.parse(localStorage.getItem("todos"));
-
-function saveTodo() {
-    var todo = document.querySelector(".plannerText").value;
-
-    localStorage.setItem("todos", JSON.stringify(todo));
-}
-
-container.addEventListener("click", function (event) {
-    event.preventDefault();
-    var btn = event.target;
-    if (btn.matches("button")) {
-        saveTodo();
-        renderTodo();
-    }
-
-})
-
-function renderTodo() {
-    // console.log(JSON.parse(localStorage.getItem("todos")));
-}
-
+function init() {
+    checkTime();
+    renderTodoList();
+};
 
 function checkTime() {
+
     for (var i = 9; i <= 17; i++) {
         $(`.time${i}`)
 
-        if (i === currentHour) {
-            $(`.time${i}`).css({ 'background-color': '#ff6961', 'color': 'white' });
+        if (i === parseInt(currentHour)) {
+            $(`.time${i}`).addClass("present");
         } else if (i < currentHour) {
-            $(`.time${i}`).css({ 'background-color': '#d3d3d3', 'color': 'white' });
+            $(`.time${i}`).addClass("past");
         } else {
-            $(`.time${i}`).css({ 'background-color': '#77dd77', 'color': 'white' });
+            $(`.time${i}`).addClass("future");
         }
+
+    }
+
+};
+
+var todoList = JSON.parse(localStorage.getItem("todos")) || [];
+
+
+buttons.addEventListener("click", function (event) {
+    event.preventDefault();
+    var element = event.target
+    
+    if (element.matches(".saveBtn")) {
+        var text = element.previousSibling.previousSibling.value;
+        var id = element.previousSibling.previousSibling.attributes.index.value;
         
+        todoList = todoList.concat([{todoText: text, todoId: id}]);
+
+        localStorage.setItem("todos", JSON.stringify(todoList));
+
+    }
+});
+
+function renderTodoList() {
+
+    for (i = 0; i < todoList.length; i++) {
+        $('textarea').eq(parseInt(todoList[i].todoId)).text(todoList[i].todoText);
+
     }
 
 }
 
-checkTime();
+    
+    init();
